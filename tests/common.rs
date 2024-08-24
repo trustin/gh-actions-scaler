6c546c813572cfda2dcc4898d1ab034338893dd2
JopopScript <lssmm1230@email.com>
2024-08-24 14:34:26 +0900

    refactoring of file creation for testing

--- tests/common.rs
+++ tests/common.rs
@@ -1,10 +1,24 @@
 use std::path::{Path, PathBuf};
 
 use std::io;
-use std::io::Write;
 
-pub fn setup_create_file(filename: &str, content: &str) -> io::Result<PathBuf> {
-    let mut path = PathBuf::from("tests/resources");
+/// Creates a new file with the given filename and content, and returns a `TeardownFile`
+/// that will automatically delete the file when dropped.
+///
+/// # Panics
+///
+/// This function will panic if:
+/// - The file cannot be created
+/// - The content cannot be written to the file
+/// - There are any I/O errors during file creation or writing
+pub fn setup_file(filename: &str, content: &str) -> TeardownFile {
+    let path_buf = new_file(filename, content).unwrap_or_else(|e| panic!(
+        "Failed to create test file. filename: {}, content: {}. Error: {}", filename, content, e));
+    TeardownFile::new(path_buf)
+}
+
+fn new_file(filename: &str, content: &str) -> io::Result<PathBuf> {
+    let mut path = PathBuf::from("tests/temp");
     path.push(filename);
 
     std::fs::write(&path, content)?;
@@ -12,27 +26,24 @@ pub fn setup_create_file(filename: &str, content: &str) -> io::Result<PathBuf> {
     Ok(path)
 }
 
-pub struct TeardownFile<'a> {
-    paths: Vec<&'a Path>,
+pub struct TeardownFile {
+    path_buf: PathBuf,
 }
 
-impl<'a> TeardownFile<'a> {
-    pub fn new(path: &'a Path) -> TeardownFile {
-        TeardownFile { paths: vec![path] }
+impl TeardownFile {
+    pub fn new(path_buf: PathBuf) -> TeardownFile {
+        TeardownFile { path_buf }
     }
 
-    pub fn new_vec(paths: Vec<&'a Path>) -> TeardownFile {
-        TeardownFile { paths }
+    pub fn path(&self) -> &Path {
+        self.path_buf.as_path()
     }
 }
 
-impl<'a> Drop for TeardownFile<'a> {
+impl Drop for TeardownFile {
     fn drop(&mut self) {
-        self.paths
-            .iter()
-            .filter(|path| path.exists())
-            .for_each(|path| {
-                let _ = std::fs::remove_file(path);
-            });
+        if self.path_buf.exists() {
+            let _ = std::fs::remove_file(self.path_buf.clone());
+        }
     }
 }
--- tests/config.rs
+++ tests/config.rs
@@ -2,6 +2,26 @@ mod common;
 
 #[cfg(test)]
 mod config_tests {
+
+    fn config_content_with_token(token: &str) -> String {
+        format!(
+            "\
+github:
+  personal_access_token: {}
+  runners:
+    name_prefix: test
+    scope: repo
+    repo_url: https://github.com/foo/bar
+machines:
+  - id: machine-1
+    ssh:
+      host: 172.18.0.100
+      username: JopopScript
+      password: JopopScript",
+            token
+        )
+    }
+
     static SIMPLE_CONFIG_CONTENT: &str = "\
 github:
   personal_access_token: 1234567890
@@ -17,22 +37,19 @@ machines:
       password: JopopScript";
 
     mod success {
-        use crate::common;
-        use gh_actions_scaler::config::Config;
+        use crate::common::setup_file;
+        use config::config::Config;
 
         #[test]
         fn simple() {
             // given
-            let path_buf =
-                common::setup_create_file("success|simple", super::SIMPLE_CONFIG_CONTENT)
-                    .expect("test config file create error");
-            let path = path_buf.as_path();
+            let teardown_file = setup_file("success|simple", super::SIMPLE_CONFIG_CONTENT);
 
             // when
+            let path = teardown_file.path();
             let config = Config::try_from(path);
 
             // then
-            common::TeardownFile::new(path);
             assert!(
                 config.is_ok(),
                 "config file read fail. reason: `{:?}`",
@@ -67,22 +84,22 @@ machines:
     }
 
     mod file_read {
-        use crate::common;
-        use gh_actions_scaler::config::{Config, ConfigError};
+        use config::config::{Config, ConfigError};
         use std::io::ErrorKind;
         use std::os::unix::fs::PermissionsExt;
         use std::path::Path;
+        use crate::common::setup_file;
 
         #[test]
         fn fail_permission_denied() {
             // given
-            let path_buf = common::setup_create_file(
+            let teardown_file = setup_file(
                 "file_read|fail_permission_denied",
                 super::SIMPLE_CONFIG_CONTENT,
-            )
-            .expect("test config file create error");
-            let path = path_buf.as_path();
+            );
+            let path = teardown_file.path();
 
+            // TODO need?? window file permission test
             std::fs::set_permissions(path, std::fs::Permissions::from_mode(0o200))
                 .expect("test file permission change fail");
 
@@ -90,15 +107,12 @@ machines:
             let config = Config::try_from(path);
 
             // then
-            common::TeardownFile::new(path);
+            assert!(config.is_err());
             match config.err().unwrap() {
                 ConfigError::ReadFailure { path: _, cause } => {
-                    assert!(
-                        cause.kind() == ErrorKind::PermissionDenied,
-                        "errorKinkd is not PermissionDenied"
-                    )
+                    assert_eq!(cause.kind(), ErrorKind::PermissionDenied, "is not ReadFailure/PermissionDenied. error: {:?}", cause)
                 }
-                _ => assert!(false, "ConfigError is not ReadFailure"),
+                other_error => assert!(false, "is not ReadFailure/PermissionDenied. error: {:?}", other_error),
             }
         }
 
@@ -111,93 +125,79 @@ machines:
             let config = Config::try_from(path);
 
             // then
-            common::TeardownFile::new(path);
+            assert!(config.is_err());
             match config.err().unwrap() {
                 ConfigError::ReadFailure { path: _, cause } => {
-                    assert!(
-                        cause.kind() == ErrorKind::NotFound,
-                        "errorKinkd is not NotFound"
-                    )
+                    assert_eq!(cause.kind(), ErrorKind::NotFound, "is not ReadFailure/NotFound. error: {:?}", cause)
                 }
-                _ => assert!(false, "ConfigError is not ReadFailure"),
+                other_error => assert!(false, "is not ReadFailure/NotFound. error: {:?}", other_error),
             }
         }
     }
 
     mod resolve_environment {
-        use crate::common;
-        use gh_actions_scaler::config::{Config, ConfigError};
+        use config::config::{Config, ConfigError};
+        use crate::common::setup_file;
 
         #[test]
         fn success_brace() {
             // given
             std::env::set_var("VARIABLE_TEST1", "yes");
 
-            let content = super::config_content_with_token("${VARIABLE_TEST1}");
-            let path_buf =
-                common::setup_create_file("resolve_environment|success_brace", content.as_str())
-                    .expect("test config file create error");
-            let path = path_buf.as_path();
+            let teardown_file = setup_file(
+                "resolve_environment|success_brace",
+                super::config_content_with_token("${VARIABLE_TEST1}").as_str(),
+            );
+            let path = teardown_file.path();
 
             // when
             let config = Config::try_from(path);
 
             // then
-            common::TeardownFile::new(path);
-            assert!(
-                config.is_ok(),
-                "config parse fail error: {:?}",
-                config.err()
-            );
+            assert!(config.is_ok(), "is not ok. return error: {:?}", config.err());
             assert_eq!(config.unwrap().github.personal_access_token, "yes");
         }
 
         #[test]
         fn fail_brace() {
             // given
-            let content = super::config_content_with_token("${VARIABLE_TEST2}");
-            let path_buf =
-                common::setup_create_file("resolve_environment|fail_brace", content.as_str())
-                    .expect("test config file create error");
-            let path = path_buf.as_path();
+            let teardown_file = setup_file(
+                "resolve_environment|fail_brace",
+                super::config_content_with_token("${VARIABLE_TEST2}").as_str(),
+            );
+            let path = teardown_file.path();
 
             // when
             let config = Config::try_from(path);
 
             // then
-            common::TeardownFile::new(path);
-            assert!(config.is_err(), "config parse not fail. expect fail...");
+            assert!(config.is_err(), "config parse not fail. expect fail");
             match config.err().unwrap() {
                 ConfigError::UnresolvedEnvironmentVariable { .. } => {}
-                _ => assert!(false, "ConfigError is not UnresolvedEnvironmentVariable"),
+                other_error => assert!(false, "is not UnresolvedEnvironmentVariable. error: {:?}", other_error),
             }
         }
     }
 
     mod resolve_file_variable {
-        use crate::common;
-        use gh_actions_scaler::config::{Config, ConfigError};
+        use config::config::{Config, ConfigError};
         use std::os::unix::fs::PermissionsExt;
+        use crate::common::setup_file;
 
         #[test]
         fn success() {
             // given
-            let content = super::config_content_with_token("\"${file:test_token}\"");
-
-            let token_path_buf = common::setup_create_file("test_token", "1234567890")
-                .expect("test token file create error");
-            let token_path = token_path_buf.as_path();
-
-            let path_buf =
-                common::setup_create_file("resolve_file_variable|success", content.as_str())
-                    .expect("test config file create error");
-            let path = path_buf.as_path();
+            let teardown_token_file = setup_file("success_token", "1234567890");
+            let teardown_config_file = setup_file(
+                "resolve_file_variable|success",
+                super::config_content_with_token("\"${file:success_token}\"").as_str(),
+            );
+            let path = teardown_config_file.path();
 
             // when
             let config = Config::try_from(path);
 
             // then
-            common::TeardownFile::new_vec(vec![path, token_path]);
             assert!(
                 config.is_ok(),
                 "config parse fail error: {:?}",
@@ -209,72 +209,45 @@ machines:
         #[test]
         fn fail_permission_denied() {
             // given
-            let content = super::config_content_with_token("${file:permission_denied_token}");
-
-            let token_path_buf = common::setup_create_file("permission_denied_token", "1234567890")
-                .expect("test token file create error");
-            let token_path = token_path_buf.as_path();
-            std::fs::set_permissions(token_path, std::fs::Permissions::from_mode(0o200))
+            let teardown_token_file = setup_file("permission_denied_token", "1234567890");
+            std::fs::set_permissions(teardown_token_file.path(), std::fs::Permissions::from_mode(0o200))
                 .expect("test token file permission change fail");
 
-            let path_buf = common::setup_create_file(
-                "resolve_file_variable|fail_permission_denied",
-                content.as_str(),
-            )
-            .expect("test config file create error");
-            let path = path_buf.as_path();
+            let teardown_config_file = setup_file(
+                "resolve_file_variable|success_config",
+                super::config_content_with_token("\"${file:permission_denied_token}\"").as_str(),
+            );
+            let path = teardown_config_file.path();
 
             // when
             let config = Config::try_from(path);
 
             // then
-            common::TeardownFile::new_vec(vec![token_path, path]);
-            assert!(config.is_err(), "config parse not fail. expect fail...");
+            assert!(config.is_err(), "config parse not fail. expect fail");
             match config.err().unwrap() {
                 ConfigError::UnresolvedFileVariable { .. } => {}
-                _ => assert!(false, "ConfigError is not UnresolvedEnvironmentVariable"),
+                other_error => assert!(false, "is not UnresolvedFileVariable. error: {:?}", other_error),
             }
         }
 
         #[test]
         fn fail_not_exist() {
             // given
-            let content = super::config_content_with_token("${file:permission_denied_token}");
-
-            let path_buf =
-                common::setup_create_file("resolve_file_variable|fail_not_exist", content.as_str())
-                    .expect("test config file create error");
-            let path = path_buf.as_path();
+            let teardown_file = setup_file(
+                "resolve_file_variable|success_config",
+                super::config_content_with_token("${file:not_exist_token}").as_str(),
+            );
+            let path = teardown_file.path();
 
             // when
             let config = Config::try_from(path);
 
             // then
-            common::TeardownFile::new(path);
-            assert!(config.is_err(), "config parse not fail. expect fail...");
+            assert!(config.is_err(), "config parse not fail. expect fail");
             match config.err().unwrap() {
-                ConfigError::UnresolvedFileVariable { .. } => {}
-                _ => assert!(false, "ConfigError is not UnresolvedEnvironmentVariable"),
+                ConfigError::UnresolvedFileVariable { .. } => {},
+                other_error => assert!(false, "is not UnresolvedFileVariable. error: {:?}", other_error),
             }
         }
-    }
-
-    fn config_content_with_token(token: &str) -> String {
-        format!(
-            "\
-github:
-  personal_access_token: {}
-  runners:
-    name_prefix: test
-    scope: repo
-    repo_url: https://github.com/foo/bar
-machines:
-  - id: machine-1
-    ssh:
-      host: 172.18.0.100
-      username: JopopScript
-      password: JopopScript",
-            token
-        )
     }
 }
