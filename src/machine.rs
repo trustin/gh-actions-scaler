059134d6a22c73086a7f55bc508637a89ca635f3
Trustin Lee <t@motd.kr>
2024-12-09 23:26:30 +0900

    Retrieve the state of all runner containers
    - Added `Machine::fetch_runners()` that fetches the state of the
      self-hosted runner containers and their related metadata, which will
      be used for load-balancing and clean-up in our next commits.
    - Added a dependency to `chrono` to parse the RFC 3339 data and time
    - Fixed incorrect shell command escape logic
      - `push_str_escaped()` now escapes all potentially dangerous
        characters.
      - Refactored `ssh_exec_*()` functions so that the caller must
        explicitly escape potentially dangerous arguments.

--- src/machine.rs
+++ src/machine.rs
@@ -1,4 +1,5 @@
 use crate::config::{Config, MachineConfig};
+use chrono::{DateTime, Datelike, ParseResult, Utc};
 use log::{debug, info};
 use maplit::hashmap;
 use ssh2::Session;
@@ -20,8 +21,105 @@ impl Machine {
         }
     }
 
-    pub fn start_runner(&self, config: &Config, run_url: &str) -> Result<(), Box<dyn Error>> {
-        // Connect to the local SSH server
+    pub fn fetch_runners(&self) -> Result<Vec<RunnerInfo>, Box<dyn Error>> {
+        let (socket_addr, mut sess) = self.connect()?;
+
+        info!("[{}] Retrieving the list of runners ..", socket_addr);
+
+        let mut cmd = String::new();
+        cmd.push_str("docker container ls --all --no-trunc --filter ");
+        cmd.push_str_escaped("label=github-self-hosted-runner");
+        cmd.push_str(" --format {{.ID}} ");
+        cmd.push_str("| xargs --no-run-if-empty docker container inspect --format ");
+        cmd.push_str_escaped(
+            "{{.ID}}|{{.State.Status}}|{{.Created}}|{{.State.StartedAt}}|{{.State.FinishedAt}}",
+        );
+
+        let output = Self::ssh_exec(&socket_addr, &mut sess, &cmd)?;
+
+        // Parse the output.
+        let mut res: Vec<RunnerInfo> = vec![];
+        for line in output.lines() {
+            let fields: Vec<&str> = line.split(['|']).collect();
+            res.push(RunnerInfo {
+                container_id: fields[0].to_string(),
+                container_state: ContainerState::from(fields[1]),
+                created_at: Self::parse_timestamp(fields[2])?,
+                started_at: Self::parse_timestamp_opt(fields[3])?,
+                finished_at: Self::parse_timestamp_opt(fields[4])?,
+            });
+        }
+
+        Ok(res)
+    }
+
+    fn parse_timestamp_opt(text: &str) -> ParseResult<Option<DateTime<Utc>>> {
+        let timestamp = Self::parse_timestamp(text)?;
+        if timestamp.year() > 1970 {
+            Ok(Some(timestamp))
+        } else {
+            Ok(None)
+        }
+    }
+
+    fn parse_timestamp(text: &str) -> ParseResult<DateTime<Utc>> {
+        Ok(DateTime::parse_from_rfc3339(text)?.to_utc())
+    }
+
+    pub fn start_runner(&self, config: &Config) -> Result<(), Box<dyn Error>> {
+        let (socket_addr, mut sess) = self.connect()?;
+
+        // TODO: Make the image URL configurable.
+        const IMAGE: &str = "ghcr.io/myoung34/docker-github-actions-runner:ubuntu-focal";
+
+        // FIXME(trustin): Pull only once a day.
+        //                 Keep the timestamp in ~/.cache/gh-actions-scaler (or $XDG_CACHE_HOME/...)
+        info!(
+            "[{}] Pulling the container image '{}' ..",
+            socket_addr, IMAGE
+        );
+        let mut pull_cmd = String::new();
+        pull_cmd.push_str("docker image pull ");
+        pull_cmd.push_str_escaped(IMAGE);
+        Self::ssh_exec(&socket_addr, &mut sess, &pull_cmd)?;
+
+        info!("[{}] Pulled the container image", socket_addr);
+
+        // FIXME(trustin): Specify a unique yet identifiable container name.
+        //                 Use `docker container rename <container_id> github-self-hosted-runner-<container_id>
+        info!("[{}] Creating and starting a new container ..", socket_addr);
+        let mut run_cmd = String::new();
+        run_cmd.push_str("docker container run --detach --restart no --label ");
+        run_cmd.push_str_escaped("github-self-hosted-runner");
+        run_cmd.push_str(" --env ACCESS_TOKEN");
+        run_cmd.push_str(" --env REPO_URL=");
+        run_cmd.push_str_escaped(&config.github.runners.repo_url);
+        run_cmd.push_str(" --env RUNNER_NAME_PREFIX=");
+        run_cmd.push_str_escaped(&config.github.runners.name_prefix);
+        run_cmd.push_str(" --env RUNNER_SCOPE=");
+        run_cmd.push_str_escaped(&config.github.runners.scope);
+        run_cmd.push_str(" --env EPHEMERAL=true");
+        run_cmd.push_str(" --env UNSET_CONFIG_VARS=true ");
+        run_cmd.push_str_escaped(IMAGE);
+
+        let container_id = Self::ssh_exec_with_env(
+            &socket_addr,
+            &mut sess,
+            &hashmap! {
+                "ACCESS_TOKEN" => config.github.personal_access_token.as_str(),
+            },
+            &run_cmd,
+        )?;
+        info!(
+            "[{}] Started a new container: {}",
+            socket_addr, container_id
+        );
+
+        Ok(())
+    }
+
+    fn connect(&self) -> Result<(SocketAddr, Session), Box<dyn Error>> {
+        // Connect to the SSH server
         let socket_addr = SocketAddr::new(self.config.ssh.host.parse()?, self.config.ssh.port);
         debug!("[{}] Making a connection attempt ..", socket_addr);
         let tcp = TcpStream::connect_timeout(&socket_addr, Duration::from_secs(30))?;
@@ -53,58 +151,7 @@ impl Machine {
             return Err("Authentication failed".into());
         }
 
-        // TODO: Make the image URL configurable.
-        const IMAGE: &str = "ghcr.io/myoung34/docker-github-actions-runner:ubuntu-focal";
-
-        // FIXME(trustin): Pull only once a day.
-        //                 Keep the timestamp in ~/.cache/gh-actions-scaler (or $XDG_CACHE_HOME/...)
-        info!(
-            "[{}] Pulling the container image '{}' ..",
-            socket_addr, IMAGE
-        );
-        Self::ssh_exec(&socket_addr, &mut sess, &["docker", "image", "pull", IMAGE])?;
-
-        info!("[{}] Pulled the container image", socket_addr);
-
-        // FIXME(trustin): Specify a unique yet identifiable container name.
-        //                 Use `docker container rename <container_id> github-self-hosted-runner-<container_id>
-        info!("[{}] Creating and starting a new container ..", socket_addr);
-        let container_id = Self::ssh_exec_with_env(
-            &socket_addr,
-            &mut sess,
-            &hashmap! {
-                "ACCESS_TOKEN" => config.github.personal_access_token.as_str(),
-            },
-            &vec![
-                "docker",
-                "container",
-                "run",
-                "--detach",
-                "--label",
-                "github-self-hosted-runner",
-                "--label",
-                &format!("github-workflow-run-url={}", run_url),
-                "--env",
-                "ACCESS_TOKEN",
-                "--env",
-                &format!("REPO_URL={}", config.github.runners.repo_url),
-                "--env",
-                &format!("RUNNER_NAME_PREFIX={}", config.github.runners.name_prefix),
-                "--env",
-                &format!("RUNNER_SCOPE={}", config.github.runners.scope),
-                "--env",
-                "EPHEMERAL=true",
-                "--env",
-                "UNSET_CONFIG_VARS=true",
-                IMAGE,
-            ],
-        )?;
-        info!(
-            "[{}] Started a new container: {}",
-            socket_addr, container_id
-        );
-
-        Ok(())
+        Ok((socket_addr, sess))
     }
 
     fn passphrase_opt(&self) -> Option<&str> {
@@ -120,15 +167,18 @@ impl Machine {
         socket_addr: &SocketAddr,
         session: &mut Session,
         env: &HashMap<&str, &str>,
-        command: &Vec<&str>,
+        command: &str,
     ) -> Result<String, Box<dyn Error>> {
         let env_script_path = Self::ssh_generate_env_script(socket_addr, session, env)?;
 
         // Prepend the command that sources the environment variable script and removes it.
-        let mut cmd_with_env = vec![".", &env_script_path, "&&", "rm", &env_script_path, "&&"];
-        for arg in command {
-            cmd_with_env.push(arg);
-        }
+        let mut cmd_with_env = String::new();
+        cmd_with_env.push_str(". ");
+        cmd_with_env.push_str_escaped(&env_script_path);
+        cmd_with_env.push_str(" && rm ");
+        cmd_with_env.push_str_escaped(&env_script_path);
+        cmd_with_env.push_str(" && ");
+        cmd_with_env.push_str(command);
 
         Self::ssh_exec(socket_addr, session, &cmd_with_env)
     }
@@ -141,7 +191,7 @@ impl Machine {
         let env_script_path = Self::ssh_exec(
             socket_addr,
             session,
-            &["mktemp", "-t", "self-hosted-runner-env.XXXXXXXXXX"],
+            "mktemp -t github-self-hosted-runner-env.XXXXXXXXXX",
         )?;
 
         let mut cmd = String::new();
@@ -164,33 +214,17 @@ impl Machine {
 
         cmd.push_str("========\n");
 
-        Self::ssh_exec_noescape(socket_addr, session, cmd)?;
+        Self::ssh_exec(socket_addr, session, &cmd)?;
         Ok(env_script_path)
     }
 
     fn ssh_exec(
         socket_addr: &SocketAddr,
         session: &mut Session,
-        command: &[&str],
-    ) -> Result<String, Box<dyn Error>> {
-        // Merge the arguments into a string while escaping if necessary.
-        let mut cmd = String::new();
-        for (i, arg) in command.iter().enumerate() {
-            if i != 0 {
-                cmd.push(' ');
-            }
-            cmd.push_str_escaped(arg);
-        }
-        Self::ssh_exec_noescape(socket_addr, session, cmd)
-    }
-
-    fn ssh_exec_noescape(
-        socket_addr: &SocketAddr,
-        session: &mut Session,
-        cmd: String,
+        cmd: &str,
     ) -> Result<String, Box<dyn Error>> {
         let mut ch = session.channel_session()?;
-        ch.exec(&cmd)?;
+        ch.exec(cmd)?;
 
         let mut stdout = String::new();
         let mut stderr = String::new();
@@ -233,13 +267,52 @@ impl Machine {
     }
 }
 
+#[derive(Debug)]
+pub struct RunnerInfo {
+    container_id: String,
+    container_state: ContainerState,
+    created_at: DateTime<Utc>,
+    started_at: Option<DateTime<Utc>>,
+    finished_at: Option<DateTime<Utc>>,
+}
+
+#[derive(Debug)]
+pub enum ContainerState {
+    Created,
+    Restarting,
+    Running,
+    Removing,
+    Paused,
+    Exited,
+    Dead,
+    Unknown(String),
+}
+
+impl From<&str> for ContainerState {
+    fn from(value: &str) -> Self {
+        match value {
+            "created" => ContainerState::Created,
+            "restarting" => ContainerState::Restarting,
+            "running" => ContainerState::Running,
+            "removing" => ContainerState::Removing,
+            "paused" => ContainerState::Paused,
+            "exited" => ContainerState::Exited,
+            "dead" => ContainerState::Dead,
+            _ => ContainerState::Unknown(value.to_string()),
+        }
+    }
+}
+
 pub trait StringExt {
     fn push_str_escaped(&mut self, s: &str);
 }
 
 impl StringExt for String {
     fn push_str_escaped(&mut self, s: &str) {
-        if !s.contains(['\'', '"', ' ', '\\']) {
+        if !s.contains([
+            '\'', '"', ' ', '\\', '|', '&', '!', ';', '$', '(', ')', '[', ']', '{', '}', '<', '>',
+            '#', '`',
+        ]) {
             // No need to escape
             self.push_str(s);
             return;
